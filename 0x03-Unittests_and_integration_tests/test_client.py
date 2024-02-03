d9976d1d8afec78f9bd8d622f0ae33c33cb80939
KhotsoSelading <k.selading007@gmail.com>
2024-02-03 08:53:45 +0200

    test_client File - Khotso

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -7,60 +7,69 @@ Date: 16-01-2024
 
 import unittest
 from parameterized import parameterized, parameterized_class
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
+
+
+def requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data):
+            self.json_data = json_data
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == "https://api.github.com/orgs/google":
+        return MockResponse(org_payload)
+    elif args[0] == org_payload['repos_url']:
+        return MockResponse(repos_payload)
+    else:
+        return MockResponse({})
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test cases for the GithubOrgClient class."""
 
-    @patch('client.GithubOrgClient.get_json')
+    @patch('client.GithubOrgClient.get_json', side_effect=requests_get)
     @parameterized.expand([
         ("google",),
         ("abc",)
     ])
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, org_name):
         """Test GithubOrgClient.org method with mocked get_json."""
         client = GithubOrgClient(org_name)
-        client.org()
-        mock_get_json.assert_called_once_with(
-            f'https://api.github.com/orgs/{org_name}')
+        result = client.org()
+        self.assertEqual(result, org_payload)
 
     @patch.object(
-            GithubOrgClient, 'org',
-            return_value={'repos_url': 'http://example.com'})
-    @patch('client.GithubOrgClient.get_json')
+        GithubOrgClient, 'org',
+        return_value={'repos_url': 'http://example.com'})
+    @patch('client.GithubOrgClient.get_json', side_effect=requests_get)
     @parameterized.expand([
         ("http://example.com", {"repo1": "data1"}),
         ("http://holberton.io", {"repo2": "data2"})
     ])
-    def test_public_repos_url(self, mock_get_json_org, mock_get_json, test_url,
-                              test_payload):
+    def test_public_repos_url(self, test_url, test_payload):
         """Test GithubOrgClient._public_repos_url method with mocked get_json
         and org."""
         client = GithubOrgClient("test_org")
         result = client._public_repos_url()
-        mock_get_json_org.assert_called_once()
-        mock_get_json.assert_called_once_with("http://example.com")
         self.assertEqual(
             result, f'{test_url}?client_id=test_id&client_secret=test_secret')
 
     @patch('client.GithubOrgClient._public_repos_url',
            return_value='http://example.com')
-    @patch('client.GithubOrgClient.get_json')
+    @patch('client.GithubOrgClient.get_json', side_effect=requests_get)
     @parameterized.expand([
         ({"repo1": "data1"},),
         ({"repo2": "data2"},)
     ])
-    def test_public_repos(self, mock_get_json_url, mock_get_json,
-                          repos_payload):
+    def test_public_repos(self, repos_payload):
         """Test GithubOrgClient.public_repos method with mocked get_json and
         public_repos_url."""
         client = GithubOrgClient("test_org")
         result = client.public_repos()
-        mock_get_json_url.assert_called_once()
-        mock_get_json.assert_called_once_with('http://example.com')
         self.assertEqual(result, repos_payload)
 
     @parameterized.expand([
@@ -81,7 +90,7 @@ class TestGithubOrgClient(unittest.TestCase):
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Integration test cases for the GithubOrgClient class."""
     @classmethod
-    @patch('client.requests.get')
+    @patch('client.requests.get', side_effect=requests_get)
     def setUpClass(cls, mock_requests_get):
         """Set up the class with mock requests.get."""
         cls.get_patcher = patch('client.requests.get')
